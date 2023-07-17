3e9f27a59281767010a9225b4247ead6095c8bb0
VALE23F <valeriaaltamiranda9@gmail.com>
2023-07-17 20:44:34 -0300

    Agregamos Carrusel

---
+++ app.js
@@ -0,0 +1,14 @@
+console.log("HOLA ")
+
+/*Let numeroEntrada=10;
+
+Let numeroEntrada2=;
+
+const COMPONENT_NAME= "MI COMPONENTE";
+
+var variable=2;
+
+Let inputData= prompt("Ingrese un número" );
+alert (inputData);*/
+alert ("ingrese un numero");
+let input1 = Number (prompt("Ingrese un numero"));
